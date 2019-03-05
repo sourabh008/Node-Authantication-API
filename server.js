b40973bafd49920959cbccd14688a286c81b706e
sourabh <sourabhkamboj247@gmail.com>
2019-03-05 21:08:39 +0530

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http=require("http");
 const app=require("./app")
-var server=http.createServer(app)
-server.listen(1000,(req,res)=>{
-    console.log("server working")
-})
+var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+  });
